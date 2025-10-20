f711aa8336a4b6a54e408181fddcc7955e1bd513
Isla <islarcherjr@icloud.com>
2025-10-20 12:59:38 +0100

    1 - initial commit

---
+++ 1-string-analyzer-service/string-analyzer-service/src/main/java/org/project/stringanalyzerservice/StringAnalyzerServiceApplication.java
@@ -0,0 +1,13 @@
+package org.project.stringanalyzerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringAnalyzerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringAnalyzerServiceApplication.class, args);
+    }
+
+}
