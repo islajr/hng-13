93f1f695eda5e13d6a469739d450526b814c6e36
Isla <islarcherjr@icloud.com>
2025-10-25 16:23:07 +0100

    2 - initial commit

---
+++ 2-currency-exchange/currency-exchange-api/src/test/java/org/project/currencyexchangeapi/CurrencyExchangeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.currencyexchangeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
